920fa8ac819592ca00d5f07a067f8edc49176aee
Alexander Biryukov <alexander.biryukov@p2p.org>
2024-10-02 23:50:12 +0400

    test_Claim

--- 01-generate-merkle-tree.ts
+++ 01-generate-merkle-tree.ts
@@ -7,13 +7,20 @@ async function main() {
     try {
         const values = [
             ["0x1111111111111111111111111111111111111111", "5000000000000000000"],
-            ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
+            ["0x2222222222222222222222222222222222222222", "2500000000000000000"],
+            ["0x3333333333333333333333333333333333333333", "100500000000000000000000"]
         ];
 
         const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
 
         console.log('Merkle Root:', tree.root);
         fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+
+        for (const [i, v] of tree.entries()) {
+            console.log('Claimer:', v[0]);
+            console.log('Amount:', v[1]);
+            console.log('Proof:', tree.getProof(i));
+        }
 
         console.log('Done.')
     } catch (err) {
--- test/TestMerkleAirdrop.sol
+++ test/TestMerkleAirdrop.sol
@@ -14,19 +14,87 @@ contract TestMerkleAirdrop is Test {
 
     address deployer;
     address owner;
-    address claimer;
+
+    bytes32 root =
+        0xb2b6910fa38afa68850d650df636efb1042aab873e1f310a9463e59dbda75f4d;
+
+    Claim[3] claims;
+
+    uint256 TOTAL_ETHER = 1_000_000 ether;
 
     function setUp() public {
         vm.createSelectFork("mainnet", 20710000);
 
         deployer = makeAddr("deployer");
         owner = makeAddr("owner");
-        claimer = makeAddr("claimer");
 
-        merkleAirdrop = new MerkleAirdrop(bytes32(0));
+        claims[0] = Claim({
+            claimer: payable(0x1111111111111111111111111111111111111111),
+            amount: 5000000000000000000,
+            proof: new bytes32[](1)
+        });
+        claims[0].proof[
+                0
+            ] = 0xddb676dfe72864e938955c6fbcda77b7461c9fb09d4bcb275191a05170909e6d;
+
+        claims[1] = Claim({
+            claimer: payable(0x2222222222222222222222222222222222222222),
+            amount: 2500000000000000000,
+            proof: new bytes32[](2)
+        });
+        claims[1].proof[
+                0
+            ] = 0x9f42d0cdc75d91462576e4b33f13a3c6fa2169f7a146fe244caf55cbe3182e74;
+        claims[1].proof[
+                1
+            ] = 0xeb02c421cfa48976e66dfb29120745909ea3a0f843456c263cf8f1253483e283;
+
+        claims[2] = Claim({
+            claimer: payable(0x3333333333333333333333333333333333333333),
+            amount: 100500000000000000000000,
+            proof: new bytes32[](2)
+        });
+        claims[2].proof[
+                0
+            ] = 0xb92c48e9d7abe27fd8dfd6b5dfdbfb1c9a463f80c712b66f3a5180a090cccafc;
+        claims[2].proof[
+                1
+            ] = 0xeb02c421cfa48976e66dfb29120745909ea3a0f843456c263cf8f1253483e283;
+
+        vm.startPrank(deployer);
+        merkleAirdrop = new MerkleAirdrop(root);
+        vm.stopPrank();
+
+        vm.deal(owner, TOTAL_ETHER);
     }
 
-    function test_42() external {
+    function test_Claim() external {
+        vm.startPrank(deployer);
+        merkleAirdrop.transferOwnership(owner);
+        vm.stopPrank();
 
+        vm.startPrank(owner);
+        merkleAirdrop.acceptOwnership();
+        Address.sendValue(payable(address(merkleAirdrop)), TOTAL_ETHER);
+        vm.stopPrank();
+
+        for (uint256 i = 0; i < claims.length; i++) {
+            uint256 balanceBefore = claims[i].claimer.balance;
+
+            vm.startPrank(claims[i].claimer);
+            merkleAirdrop.claim(claims[i].amount, claims[i].proof);
+            vm.stopPrank();
+
+            uint256 balanceAfter = claims[i].claimer.balance;
+
+            assertEq(merkleAirdrop.claimed(claims[i].claimer), true);
+            assertEq(balanceAfter - balanceBefore, claims[i].amount);
+        }
     }
+}
+
+struct Claim {
+    address payable claimer;
+    uint256 amount;
+    bytes32[] proof;
 }
